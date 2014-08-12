8412f81287b8d9dfce4d72d085880d2f4352a90a
nunoguedelha <nguedelh@laas.fr>
2014-08-12 03:40:28 +0200

    torseurs complete

---
+++ figs/ipe2pdf.bat
@@ -0,0 +1,7 @@
+Title ipe2pdf
+
+Echo "convert all .ipe file to .pdf"
+
+"C:\Program Files (x86)\ipe-7.1.4\bin\ipetoipe.exe" -pdf  D:\Stage\Rapport\figs\figures.ipe
+"C:\Program Files (x86)\ipe-7.1.4\bin\ipetoipe.exe" -pdf  D:\Stage\Rapport\figs\torseurs.ipe
+exit
