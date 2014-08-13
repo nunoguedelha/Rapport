dd6b5a5bc0b98789bc2e846b4bfed05fb33b8dc7
nunoguedelha <nguedelh@laas.fr>
2014-08-13 05:24:54 +0200

    etape 1

--- figs/ipe2pdf.bat
+++ figs/ipe2pdf.bat
@@ -4,4 +4,5 @@ Echo "convert all .ipe file to .pdf"
 
 "C:\Program Files (x86)\ipe-7.1.4\bin\ipetoipe.exe" -pdf  D:\Stage\Rapport\figs\figures.ipe
 "C:\Program Files (x86)\ipe-7.1.4\bin\ipetoipe.exe" -pdf  D:\Stage\Rapport\figs\torseurs.ipe
+"C:\Program Files (x86)\ipe-7.1.4\bin\ipetoipe.exe" -pdf  D:\Stage\Rapport\figs\hybridDynamics4etapes.ipe
 exit
